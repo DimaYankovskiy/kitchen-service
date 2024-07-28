7b75dcef426ce881dbb70254b892eaecf34dae90
Dima <dima2000yan@gmail.com>
2024-07-28 23:49:29 +0300

    DishType, Ingredient list views implemented

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
-from kitchen.views import temporary
-
-path(
-    "", temporary, name="temporary"
-),
+from kitchen.views import temporary, DishTypeListView, IngredientListView
 
 urlpatterns = [
     path("", temporary, name="temporary"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("ingredients/", IngredientListView.as_view(), name="ingredient-list"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from kitchen.models import DishType, Ingredient
 
 
 def temporary(request):
     return render(request, "base.html")
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_type_list.html"
+    success_url = reverse_lazy("kitchen:dish-type-list")
+
+
+class IngredientListView(generic.ListView):
+    model = Ingredient
+    template_name = "kitchen/ingredient_list.html"
+    success_url = reverse_lazy("kitchen:ingredient-list")
