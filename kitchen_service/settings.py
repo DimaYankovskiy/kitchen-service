8881a4be30ae459fa332c3dcc06715d5e6f40df6
Dima <dima2000yan@gmail.com>
2024-07-31 20:02:17 +0300

    secret key removed

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",
-                            "django-insecure-o)d&@waf+4hdgf8i*)9l13!-3q+0if3zkp#=%u)0=+!_=*9cv$")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG") != "False"
