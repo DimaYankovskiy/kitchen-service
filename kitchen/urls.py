ac6d36b074a04f71e0e0a758bc1afb1b9a225d58
Dima <dima2000yan@gmail.com>
2024-08-02 14:14:44 +0300

    url patterns changes

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -18,7 +18,11 @@ from kitchen.views import (
 )
 
 urlpatterns = [
-    path("dish-types/", DishTypeListView.as_view(), name="dish_type-list"),
+    path(
+        "dish-types/",
+        DishTypeListView.as_view(),
+        name="dish_type-list"
+    ),
     path(
         "dish-types/create/",
         DishTypeCreateView.as_view(),
@@ -34,8 +38,11 @@ urlpatterns = [
         DishTypeDeleteView.as_view(),
         name="dish_type-delete"
     ),
-
-    path("ingredients/", IngredientListView.as_view(), name="ingredient-list"),
+    path(
+        "ingredients/",
+        IngredientListView.as_view(),
+        name="ingredient-list"
+    ),
     path(
         "ingredient/create/",
         IngredientCreateView.as_view(),
@@ -51,10 +58,21 @@ urlpatterns = [
         IngredientDeleteView.as_view(),
         name="ingredient-delete"
     ),
-
-    path("", DishListView.as_view(), name="dish-list"),
-    path("dish/<int:pk>/", DishDetailedView.as_view(), name="dish-detail"),
-    path("dish/create/", DishCreateView.as_view(), name="dish-create"),
+    path(
+        "",
+        DishListView.as_view(),
+        name="dish-list"
+    ),
+    path(
+        "dish/<int:pk>/",
+        DishDetailedView.as_view(),
+        name="dish-detail"
+    ),
+    path(
+        "dish/create/",
+        DishCreateView.as_view(),
+        name="dish-create"
+    ),
     path(
         "dish/<int:pk>/update/",
         DishUpdateView.as_view(),
@@ -65,10 +83,16 @@ urlpatterns = [
         DishDeleteView.as_view(),
         name="dish-delete"
     ),
-
-
-    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(),
+        name="login"
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout"
+    ),
 ]
 
 app_name = "kitchen"
